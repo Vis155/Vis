532f4592e9814eeb821af5465b5363f767080ec3
Vis155 <1550772545@qq.com>
2018-07-16 10:17:54 +0800

    7.16 by vis

---
+++ src/main/java/com/vis/demo/Student.java
@@ -0,0 +1,9 @@
+package com.vis.demo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String name;
+    private Integer age;
+}
---
+++ src/main/java/com/vis/demo/Test.java
@@ -0,0 +1,10 @@
+package com.vis.demo;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("vis");
+        System.out.println(student);
+    }
+}
